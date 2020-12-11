a7e41a78b4e40c0ab9f0f4085a35b777234f875e
Emir <pooky03@gmail.com>
2020-12-11 16:28:38 -0600

    helper functions

---
+++ src/helpers.js
@@ -0,0 +1,36 @@
+export const isEmail = (email) => {
+  const regex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return regex.test(email);
+};
+
+export const isAuth = ({ email, password }) => {
+  return email === 'emir@wndyr.com' && password === 'wndyr';
+};
+
+export const VARS = {
+  email: 'email',
+  password: 'password',
+};
+
+export const initialState = {
+  [VARS.email]: '',
+  [VARS.password]: '',
+};
+
+// export const isFilled = (state) =>
+//   !Object.values(state).every((prop) => prop !== '') || !isEmail(state.email);
+
+// export const handler = (state, setState) => (field, event) =>
+//   setState({ ...state, [field]: event.target.value });
+
+export function isFilled(state) {
+  return (
+    !Object.values(state).every((prop) => prop !== '') || !isEmail(state.email)
+  );
+}
+
+export function handler(state, setState) {
+  return function (field, event) {
+    return setState({ ...state, [field]: event.target.value });
+  };
+}
